4fc46ba98371e25f561cbadab428a40fea23e658
Thomas B. Preußer <thomas.preusser@tu-dresden.de>
2015-02-11 15:01:09 +0100

    Fixed Iterator.

--- interfaces/Iterator.h
+++ interfaces/Iterator.h
@@ -6,17 +6,14 @@
 namespace q2d {
 namespace quantor {
 
-// TODO the iterator needs to know if it has ownership of the iterated structure
-// if this is the case it has to delete the owned structure
-
 /**
  *  This class has been introduced for TBP-code compatibility.
  */
 template<typename ElementType>
 class Iterator {
 private:
-    ElementType const* m_current;
-    ElementType const* m_end;
+    ElementType const *       m_current;
+    ElementType const *const  m_end;
 
 public:
 
@@ -28,19 +25,20 @@ public:
         Q_CHECK_PTR(end);
     }
 
-    operator bool() {
+    operator bool() const {
         return m_current != m_end;
     }
     ElementType const &operator*() {
-        return m_current;
+        return *m_current;
     }
-    ElementType const* operator++() {
-        return m_current++;
+    Iterator<ElementType> operator++() {
+	Iterator<ElementType> const  old = *this;
+	m_current++;
+        return  old;
     }
 };
 
 } // namespace quantor
 } // namespace q2d
-
 
 #endif // ITERATOR_H
